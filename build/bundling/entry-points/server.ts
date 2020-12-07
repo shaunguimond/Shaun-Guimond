342b857c33764413bed94784c5b9e0e5b872fd17
Shaun Guimond <shaun.guimond@outlook.com>
2020-12-07 15:13:14 -0800

    updated and reinitialized repo.

--- build/bundling/entry-points/server.ts
+++ build/bundling/entry-points/server.ts
@@ -2,7 +2,7 @@ import server from "@frontity/core/src/server";
 import twentynineteen_theme_default from "twentynineteen-theme/src/index";
 import frontity__wp_source_default from "@frontity/wp-source/src/index";
 import frontity__tiny_router_default from "@frontity/tiny-router/src/index";
-import frontity__html2react_default from "@frontity/html2react/src/server";
+import frontity__html2react_default from "@frontity/html2react/src/index";
 import frontity__head_tags_default from "@frontity/head-tags/src/index";
 
 const packages = {
--- build/bundling/entry-points/shaun-guimond/client.ts
+++ build/bundling/entry-points/shaun-guimond/client.ts
@@ -2,7 +2,7 @@ import client from "@frontity/core/src/client";
 import twentynineteen_theme_default from "twentynineteen-theme/src/index";
 import frontity__wp_source_default from "@frontity/wp-source/src/index";
 import frontity__tiny_router_default from "@frontity/tiny-router/src/index";
-import frontity__html2react_default from "@frontity/html2react/src/client";
+import frontity__html2react_default from "@frontity/html2react/src/index";
 import frontity__head_tags_default from "@frontity/head-tags/src/index";
 
 const packages = {
@@ -22,7 +22,7 @@ if (module["hot"]) {
       "twentynineteen-theme/src/index",
       "@frontity/wp-source/src/index",
       "@frontity/tiny-router/src/index",
-      "@frontity/html2react/src/client",
+      "@frontity/html2react/src/index",
       "@frontity/head-tags/src/index",
     ],
     () => {
@@ -30,7 +30,7 @@ if (module["hot"]) {
       const twentynineteen_theme_default = require("twentynineteen-theme/src/index").default;
       const frontity__wp_source_default = require("@frontity/wp-source/src/index").default;
       const frontity__tiny_router_default = require("@frontity/tiny-router/src/index").default;
-      const frontity__html2react_default = require("@frontity/html2react/src/client").default;
+      const frontity__html2react_default = require("@frontity/html2react/src/index").default;
       const frontity__head_tags_default = require("@frontity/head-tags/src/index").default;
       const packages = {
         twentynineteen_theme_default,
